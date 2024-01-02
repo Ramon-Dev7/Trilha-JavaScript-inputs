e2b28d987f6cd05a22f7b6eaee52ef2badbf43b2
Ramon-Santos-devaprendo-aprender <ssilvaramon2022@gmail.com>
2024-01-02 16:00:22 -0300

    Upload arquivos JavaScript Inputs

---
+++ index.js
@@ -0,0 +1,9 @@
+import entradaDados from 'readline-sync';
+const nome = entradaDados.question('Digite seu nome: ');
+
+console.log('Olá, '+nome);
+
+import entradaDados from 'readline-sync';
+
+let idade = entradaDados .question('Digite sua idade: ');
+console.log("Você tem "+idade+" anos de idade");
